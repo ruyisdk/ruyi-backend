0d6e296867f4417bad812937ad0a5efa3ad6723e
WANG Xuerui <git@xen0n.name>
2024-12-31 00:11:59 +0800

    feat: implement the telemetry upload endpoint

--- ruyi_backend/app/telemetry.py
+++ ruyi_backend/app/telemetry.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from sqlalchemy import text
 
 from ..db.conn import DIMainDB
 from ..schema.client_telemetry import UploadPayload
@@ -12,9 +13,37 @@ async def telemetry_pm_upload_v1(payload: UploadPayload, main_db: DIMainDB) -> N
         raise ValueError("Invalid telemetry format version")
 
     async with main_db.connect() as conn:
-        # TODO: de-dupe events with payload.nonce
-        # TODO: record ruyi_version and installation info independently
-        # TODO: record the aggregated events
-        pass
+        # Record payload.ruyi_version into telemetry_ruyi_versions
+        await conn.execute(
+            text("INSERT INTO `telemetry_ruyi_versions` (version) VALUES (:version)"),
+            {"version": payload.ruyi_version},
+        )
+
+        # Record payload.installation into telemetry_raw_installation_infos
+        if ins := payload.installation:
+            await conn.execute(
+                text(
+                    "INSERT IGNORE INTO `telemetry_raw_installation_infos` (`report_uuid`, `raw`) VALUES (:report_uuid, :raw)"
+                ),
+                {
+                    "report_uuid": ins.report_uuid,
+                    "raw": ins.model_dump_json(),
+                },
+            )
+
+        # Record the raw aggregated events into telemetry_raw_uploads
+        # De-duping is achieved by using INSERT IGNORE INTO and the unique
+        # constraint on the nonce column
+        await conn.execute(
+            text(
+                "INSERT IGNORE INTO `telemetry_raw_uploads` (`nonce`, `raw_events`) VALUES (:nonce, :raw_events)"
+            ),
+            {
+                "nonce": payload.nonce,
+                "raw_events": payload.model_dump_json(),
+            },
+        )
+
+        await conn.commit()
 
     return None
