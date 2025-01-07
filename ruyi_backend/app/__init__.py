c07b5fd6b5d776754310ef1b19521be742787099
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-01-07 23:13:22 +0800

    feat: hook up the frontend apis

--- ruyi_backend/app/__init__.py
+++ ruyi_backend/app/__init__.py
@@ -1,8 +1,10 @@
 from .root import app as app
 
 # register the various API endpoints
+from . import frontend as frontend
 from . import misc as misc
 from . import telemetry as telemetry
 
+app.include_router(frontend.router)
 app.include_router(misc.router)
 app.include_router(telemetry.router)
