32ab017fd39f7b1227e5d508ecf8472651b78c50
WANG Xuerui <git@xen0n.name>
2025-02-25 10:50:20 +0800

    feat: wire up frontend dashboard to DB

--- ruyi_backend/app/frontend.py
+++ ruyi_backend/app/frontend.py
@@ -1,31 +1,23 @@
+import asyncio
+
 from fastapi import APIRouter
 
+from ..components.frontend_dashboard_processor import crunch_dashboard_numbers
 from ..db.conn import DIMainDB
+from ..es import DIMainES
 from ..schema.frontend import DashboardDataV1, DashboardEventDetailV1
 
 router = APIRouter()
 
 
 @router.post("/fe/dashboard")
-async def get_dashboard_data_v1(main_db: DIMainDB) -> DashboardDataV1:
-    # TODO: query the crunched numbers
-    _ = main_db
+async def get_dashboard_data_v1(
+    main_db: DIMainDB,
+    main_es: DIMainES,
+) -> DashboardDataV1:
+    # crunch the semi-processed numbers
+    # TODO: put these into periodic background tasks and cache the result
+    async with main_db.connect() as db:
+        x = await crunch_dashboard_numbers(db, main_es)
 
-    # this is placeholder data
-    top_pkgs = {
-        "toolchain/gnu-milkv-milkv-duo-musl-bin": DashboardEventDetailV1(total=4),
-        "toolchain/gnu-upstream": DashboardEventDetailV1(total=3),
-        "toolchain/gnu-plct": DashboardEventDetailV1(total=2),
-        "board-image/buildroot-sdk-milkv-duo": DashboardEventDetailV1(total=1),
-    }
-    top_cmds = {
-        "riscv64-unknown-linux-musl-gcc": DashboardEventDetailV1(total=100),
-        "riscv64-unknown-linux-musl-g++": DashboardEventDetailV1(total=50),
-        "riscv64-unknown-linux-musl-ld": DashboardEventDetailV1(total=5),
-    }
-    return DashboardDataV1(
-        downloads=DashboardEventDetailV1(total=20),
-        installs=DashboardEventDetailV1(total=5),
-        top_packages=top_pkgs,
-        top_commands=top_cmds,
-    )
+    return x
---
+++ ruyi_backend/components/frontend_dashboard_processor.py
@@ -0,0 +1,87 @@
+import datetime
+
+from elasticsearch import AsyncElasticsearch
+from sqlalchemy.ext.asyncio import AsyncConnection
+from sqlalchemy.sql.expression import func, select
+
+from ..db.schema import (
+    telemetry_aggregated_events,
+    telemetry_installation_infos,
+)
+from ..schema.frontend import DashboardDataV1, DashboardEventDetailV1
+
+
+async def crunch_dashboard_numbers(
+    db: AsyncConnection,
+    es: AsyncElasticsearch,
+) -> DashboardDataV1:
+    """
+    Ingests the semi-processed telemetry events to produce statistics for the
+    RuyiSDK website dashboard.
+    """
+
+    # query download counts from ES
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    download_count_resp = await es.count(
+        query={
+            "bool": {
+                "must": [
+                    {"wildcard": {"url.path": {"value": "/ruyisdk/*"}}},
+                    {
+                        "range": {
+                            "@timestamp": {
+                                "gte": "2025-01-01T00:00:00+08:00",
+                                "lt": now.isoformat(),
+                            }
+                        }
+                    },
+                ]
+            }
+        }
+    )
+    download_count = download_count_resp["count"]
+
+    # count total installations
+    installation_count = await db.scalar(
+        select(func.count(1)).select_from(telemetry_installation_infos)
+    )
+
+    # count invocations grouped by individual ruyi commands
+    command_counts: dict[str, int] = {}
+    async for ev in await db.stream(
+        select(
+            telemetry_aggregated_events.c.params_kv_raw,
+            telemetry_aggregated_events.c.count,
+        ).where(
+            telemetry_aggregated_events.c.kind == "cli:invocation-v1",
+        ),
+    ):
+        kv = {k: v for k, v in ev[0]}  # [["key", "xxx"], ] => {"key": "xxx"}
+        count = ev[1]
+
+        cmd = kv.get("key")
+        if cmd is None:
+            continue
+
+        cmd = "ruyi" if cmd == "<bare>" else f"ruyi {cmd}"
+        command_counts[cmd] = command_counts.get(cmd, 0) + count
+
+    sorted_command_counts = dict(
+        sorted(
+            command_counts.items(),
+            key=lambda x: x[1],
+            reverse=True,
+        )
+    )
+
+    top10_sorted_commands = {
+        k: DashboardEventDetailV1(total=v)
+        for k, v in list(sorted_command_counts.items())[:10]
+    }
+
+    return DashboardDataV1(
+        downloads=DashboardEventDetailV1(total=download_count),
+        installs=DashboardEventDetailV1(total=installation_count),
+        top_packages={},  # TODO: numbers are not reported yet
+        top_commands=top10_sorted_commands,
+    )
