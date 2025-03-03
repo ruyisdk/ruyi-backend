db3802d6ad6a28a75ebed799429020b9c3c6f611
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-03-03 18:09:25 +0800

    style: ruff format

--- ruyi_backend/components/telemetry_processor.py
+++ ruyi_backend/components/telemetry_processor.py
@@ -78,7 +78,9 @@ async def process_telemetry_data(
 
     if installation_infos_buffer:
         await conn.execute(
-            insert(telemetry_installation_infos).values(list(installation_infos_buffer.values()))
+            insert(telemetry_installation_infos).values(
+                list(installation_infos_buffer.values())
+            )
         )
 
     if riscv_machine_infos_buffer:
