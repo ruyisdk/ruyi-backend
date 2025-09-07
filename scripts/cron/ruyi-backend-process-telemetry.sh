411ce1166d95e3ae33e918351d63cb8ea01d5524
WANG Xuerui <wangxuerui@iscas.ac.cn>
2025-09-07 20:45:04 +0800

    feat: add example telemetry processing cronjob
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

---
+++ scripts/cron/ruyi-backend-process-telemetry.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+: "${RUYISDK_API_HOST:?RUYISDK_API_HOST must be set}"
+: "${RUYISDK_API_USERNAME:?RUYISDK_API_USERNAME must be set}"
+: "${RUYISDK_API_PASSWORD:?RUYISDK_API_PASSWORD must be set}"
+
+time_start="2024-04-10T00:00:00+08:00"
+time_end="$(date -Iseconds)"
+
+do_login() {
+	curl "$RUYISDK_API_HOST"/oauth2/token \
+		-s \
+		-X POST \
+		-H 'Content-Type: application/x-www-form-urlencoded' \
+		--data-raw "grant_type=password&username=${RUYISDK_API_USERNAME}&password=${RUYISDK_API_PASSWORD}"
+}
+
+get_accesstoken() {
+	do_login | jq -r '.access_token'
+}
+
+ACCESS_TOKEN="$(get_accesstoken)"
+
+common_args=(
+	-s
+	-H 'Content-Type: application/json'
+	-H "Authorization: Bearer $ACCESS_TOKEN"
+)
+
+curl "$RUYISDK_API_HOST"/admin/process-telemetry-v1 \
+	"${common_args[@]}" \
+	--data-raw "{\"time_start\":\"${time_start}\",\"time_end\":\"${time_end}\"}"
+
+curl "$RUYISDK_API_HOST"/admin/refresh-github-stats-v1 \
+	"${common_args[@]}" \
+	--data-raw '{}'
+
+curl "$RUYISDK_API_HOST"/admin/refresh-repo-news-v1 \
+	"${common_args[@]}" \
+	--data-raw '{}'
+
+hour="$(date +%H)"
+if [[ $hour -eq 8 ]]; then
+	curl "$RUYISDK_API_HOST"/admin/refresh-pypi-stats-v1 \
+		"${common_args[@]}" \
+		--data-raw '{}'
+fi
