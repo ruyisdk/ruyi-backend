8460b12f7e31f8fecbf17ac058607e5a4c336871
WANG Xuerui <wangxuerui@iscas.ac.cn>
2026-08-27 15:17:08 +0800

    refactor: mask some ruff lints for now
    Signed-off-by: WANG Xuerui <wangxuerui@iscas.ac.cn>

--- ruyi_backend/app/__init__.py
+++ ruyi_backend/app/__init__.py
@@ -1,5 +1,5 @@
 # this ordering is required to avoid circular import issues
-from .root import app as app  # noqa: I001
+from .root import app as app
 
 # register the various API endpoints
 from . import admin as admin
